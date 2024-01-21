423abe4ee2bb6307b481c97819508234dcf5a545
Hieu <hieuvo@gmail.com>
2024-01-21 22:08:02 +0700

    Form Mượn sách

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, map, of } from 'rxjs';
 import { Book } from 'src/app/models/book';
@@ -6,6 +7,8 @@ import { SearchModel } from 'src/app/models/search-model';
 import { AlertService, AlertType } from 'src/app/services/alert.service';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
 import { HttpService } from 'src/app/services/http.service';
+import { convertJSToCSDate, convertToJSDateString } from 'src/app/models/Utils';
+
 
 @Component({
   selector: 'app-book-details',
@@ -14,16 +17,28 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class BookDetailsComponent
 {
+  protected userId: string | undefined = undefined;
+  protected cardNumber: string | undefined = undefined;
   protected book: Book = new Book();
   protected publishDate: string | undefined = undefined;
   protected isFavorite: boolean = false;
   protected isInCart: boolean = false;
   protected waitingForFavoriteAction = false;
   protected waitingForCartAction = false;
+  protected waitingForBorrowAction = false;
 
   protected inCategory: Observable<Book[]> = of([]);
   protected fromAuthor: Observable<Book[]> = of([]);
   protected fromPublisher: Observable<Book[]> = of([]);
+
+  protected borrowDate: FormControl = new FormControl(convertToJSDateString(new Date()), [Validators.required]);
+  protected returnDate: FormControl = new FormControl(null, [Validators.required]);
+
+
+  protected borrowForm: FormGroup = new FormGroup({
+    borrowDate: this.borrowDate,
+    returnDate: this.returnDate,
+  });
 
   constructor(
     private httpService: HttpService,
@@ -124,5 +139,44 @@ export class BookDetailsComponent
   removeCart ()
   {
     
+  }
+
+  borrowEvent ()
+  {
+    if (!this.userId)
+    {
+      this.router.navigate(['/login']);
+      return;
+    }
+    if (!this.cardNumber)
+    {
+      this.alertService.appendAlert('Vui lòng liên kết thẻ thư viện trước khi mượn sách', AlertType.danger, 10, 'alert-container');
+      return;
+    }
+  }
+
+  borrow ()
+  {
+    this.waitingForBorrowAction = true;
+    const formData = this.borrowForm.value;
+    const data = {
+      cardNumber: this.cardNumber,
+      bookId: this.book._id,
+      borrowDate: convertJSToCSDate(formData.borrowDate),
+      returnDate: convertJSToCSDate(formData.returnDate)
+    }
+    console.log(data);
+    
+    this.httpService.borrow(data).subscribe({
+      next: res =>
+      {
+        this.waitingForBorrowAction = false;
+        this.alertService.appendAlert('Đăng ký mượn thành công, mang thẻ đến thư viện để nhận sách', AlertType.success, 10, 'alert-container');
+      }, error: err =>
+      {
+        this.waitingForBorrowAction = false;
+        this.alertService.appendAlert('Đã xảy ra lỗi, vui lòng thử lại sau', AlertType.danger, 5, 'alert-container');
+      }
+    });
   }
 }
