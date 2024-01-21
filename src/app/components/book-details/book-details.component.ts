9e8f10fbcf7bab37efff3a0d35e88ed0ff42ce94
Hieu <hieuvo@gmail.com>
2024-01-21 14:11:55 +0700

    Fix Component book detail

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, map, of } from 'rxjs';
 import { Book } from 'src/app/models/book';
@@ -15,26 +14,24 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class BookDetailsComponent
 {
-  protected userId: string | undefined = undefined;
-  protected cardNumber: string | undefined = undefined;
   protected book: Book = new Book();
+  protected publishDate: string | undefined = undefined;
   protected isFavorite: boolean = false;
-  protected waitingForBorrowAction = false;
+  protected waitingForFavoriteAction = false;
 
   protected inCategory: Observable<Book[]> = of([]);
   protected fromAuthor: Observable<Book[]> = of([]);
   protected fromPublisher: Observable<Book[]> = of([]);
 
-  protected borrowDate: FormControl = new FormControl(new Date().toLocaleDateString(), [Validators.required]);
-  protected returnDate: FormControl = new FormControl(null, [Validators.required]);
-
-  protected borrowForm: FormGroup = new FormGroup({
-    borrowDate: this.borrowDate,
-    returnDate: this.returnDate,
-  });
-
-  constructor(private httpService: HttpService, private authGuardService: AuthGuardService, private route: ActivatedRoute, private router: Router, private alertService: AlertService)
+  constructor(
+    private httpService: HttpService,
+    private authGuardService: AuthGuardService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private alertService: AlertService
+  )
   {
+    this.waitingForFavoriteAction = true;
     this.route.paramMap.subscribe(params =>
     {
       const id = params.get('id') || '';
@@ -47,19 +44,18 @@ export class BookDetailsComponent
 
       this.httpService.getBooks(id).subscribe(book =>
       {
-        this.book = new Book(book.id, book.title, book.category, book.image, book.author, book.publisher, book.publishDate || 'Không rõ', book.overview, book.numberOfPages);
-
-        console.log(book);
-
+        this.book = book;
+        this.publishDate = new Date(book.publishDate).toLocaleDateString();
 
         if (authGuardService.isLoggedIn)
           authGuardService.userData.subscribe({
             next: res =>
             {
-              this.userId = res.id;
-              this.cardNumber = res.libraryCard.id;
+              this.httpService.isFavorite(this.book._id).subscribe(fav => this.isFavorite = fav);             
             }
           });
+
+        this.waitingForFavoriteAction = false;
 
         categorySearch.category = this.book.category;
         authorSearch.author = this.book.author;
@@ -72,42 +68,49 @@ export class BookDetailsComponent
     });
   }
 
-  borrowEvent ()
+  addFavorite ()
   {
-    if (!this.userId)
+    if (!this.authGuardService.isLoggedIn)
     {
       this.router.navigate(['/login']);
       return;
     }
-    if (!this.cardNumber)
-    {
-      this.alertService.appendAlert('Vui lòng liên kết thẻ thư viện trước khi mượn sách', AlertType.danger, 10, 'alert-container');
-      return;
-    }
-  }
 
-  borrow ()
-  {
-    this.waitingForBorrowAction = true;
-    const formData = this.borrowForm.value;
-    const data = {
-      cardNumber: this.cardNumber,
-      bookId: this.book._id,
-      borrowDate: formData.borrowDate,
-      returnDate: formData.returnDate
-    }
-    console.log(data);
-
-    this.httpService.borrow(data).subscribe({
+    this.waitingForFavoriteAction = true;
+    this.httpService.addFavorite(this.book._id).subscribe({
       next: res =>
       {
-        this.waitingForBorrowAction = false;
-        this.alertService.appendAlert('Đăng ký mượn thành công, mang thẻ đến thư viện để nhận sách', AlertType.success, 10, 'alert-container');
+        this.waitingForFavoriteAction = false;
+        this.isFavorite = true;
       }, error: err =>
       {
-        this.waitingForBorrowAction = false;
+        this.waitingForFavoriteAction = false;
         this.alertService.appendAlert('Đã xảy ra lỗi, vui lòng thử lại sau', AlertType.danger, 5, 'alert-container');
       }
     });
   }
+
+  removeFavorite ()
+  {
+    if (!this.authGuardService.isLoggedIn)
+    {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.waitingForFavoriteAction = true;
+    this.httpService.removeFavorite(this.book._id).subscribe({
+      next: res =>
+      {
+        this.waitingForFavoriteAction = false;
+        this.isFavorite = false;
+      }, error: err =>
+      {
+        this.waitingForFavoriteAction = false;
+        this.alertService.appendAlert('Đã xảy ra lỗi, vui lòng thử lại sau', AlertType.danger, 5, 'alert-container');
+      }
+    });
+  }
+ 
+  
 }
